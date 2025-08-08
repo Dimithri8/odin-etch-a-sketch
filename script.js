9177b265ffe8e59611cc1ab1fd708cf71c0dc1ef
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-08 13:01:20 +0530

    Developed a function sketch section

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+document.body.style.padding = "0px";
+document.body.style.margin = "0px";
+document.body.style.height = "100vh";
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.flexDirection = "column-reverse";
+document.body.style.alignItems = "center";
+document.body.style.gap = "10px";
 
+const container = document.querySelector("#container");
+
+container.style.display = "flex";
+container.style.width = "550px";
+container.style.flexWrap = "wrap";
+
+const newGridBtn = document.createElement("button");
+newGridBtn.textContent = "Create New Grid";
+newGridBtn.classList.add("primary-button");
+newGridBtn.style.color = "white";
+newGridBtn.style.backgroundColor = "#f54275";
+newGridBtn.style.border = "none";
+newGridBtn.style.padding = "10px";
+newGridBtn.style.borderRadius = "10px";
+
+document.body.appendChild(newGridBtn);
+
+function createSquareDiv() {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.style.height = "30px";
+  square.style.width = "30px";
+  square.style.backgroundColor = "#e4e8f0";
+  square.style.border = "2px solid white";
+  square.style.cursor = "pointer";
+
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "grey";
+  });
+
+  container.appendChild(square);
+}
+
+for (let i = 1; i <= 256; i++) {
+  createSquareDiv();
+}
